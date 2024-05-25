cd7aa8e340bb18d58536d7644cdd557b80be63f4
XeindsTwo <125365638+XeindsTwo@users.noreply.github.com>
2024-05-25 08:47:10 +0300

    Валидация, рефакторинг кода js

--- app/js/main.js
+++ app/js/main.js
@@ -1,9 +1,11 @@
 import {bindScrollToLinks} from './gsap.js';
 import {bindShareLinks} from './copy.js';
+import {setupMobileMenu} from "./mobileMenu.js";
 import {animateCompetitionSection} from './animateCompetitionSection.js';
 
 bindScrollToLinks();
 bindShareLinks();
+setupMobileMenu();
 animateCompetitionSection();
 
 new Swiper('.prizes__swiper', {
@@ -113,43 +115,3 @@ ScrollTrigger.create({
     });
   }
 });
-
-const html = document.documentElement;
-const menuBtn = document.querySelector('.menu-btn');
-const headerMobile = document.querySelector('.header__mobile');
-const anchors = document.querySelectorAll('a.header__link.mobile');
-
-menuBtn.addEventListener('click', () => {
-  menuBtn.blur();
-  html.classList.toggle('active');
-  menuBtn.classList.toggle('active');
-  headerMobile.classList.toggle('active');
-});
-
-function scrollToTarget(targetId) {
-  const targetSection = document.querySelector(targetId);
-  if (targetSection) {
-    html.classList.remove('active');
-    headerMobile.classList.remove('active');
-    menuBtn.classList.remove('active');
-    setTimeout(() => {
-      const targetOffset = targetSection.offsetTop - 25;
-      window.scrollTo({top: targetOffset, behavior: 'smooth'});
-    }, 700);
-  }
-}
-
-function handleAnchorClick(event) {
-  event.preventDefault();
-  const href = this.getAttribute('href');
-  const hrefParts = href.split('#');
-  if (hrefParts.length === 2) {
-    const targetId = '#' + hrefParts[1];
-    scrollToTarget(targetId);
-  }
-}
-
-for (const anchor of anchors) {
-  anchor.addEventListener('click', handleAnchorClick);
-  anchor.addEventListener('touchstart', handleAnchorClick, {passive: true});
-}
